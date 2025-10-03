fc47a248677b609cc12e5ccd2258c098bd5e9a24
Lans12345 <olanalans12345@gmail.com>
2025-10-03 11:28:00 +0800

    fixed auto login

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,7 @@ class _MyAppState extends State<MyApp> {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/onboarding',
+      initialRoute: '/splash',
       getPages: [
         GetPage(name: '/onboarding', page: () => const OnboardingScreen()),
         GetPage(name: '/splash', page: () => const SplashScreen()),
